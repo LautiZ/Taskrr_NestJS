7642b2d5cb52bbd84513fd7c068b527c3485283b
LautiZ <zullolau@gmail.com>
2024-09-12 14:05:47 -0300

    Configuración, variables de entorno y configuracion de CORS.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors(CORS);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+  console.log(`💠 App runing on port ${configService.get('PORT')}`);
+  console.log(`💠 Url: ${await app.getUrl()}`);
 }
 bootstrap();
