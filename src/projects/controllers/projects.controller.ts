ffbb8c807b2a16aff6228abd321a3de2fcf4c0e0
LautiZ <zullolau@gmail.com>
2024-09-13 19:46:18 -0300

    Add - Controladores de users y projects

--- src/projects/controllers/projects.controller.ts
+++ src/projects/controllers/projects.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  // Post,
+} from '@nestjs/common';
 import { ProjectsService } from '../services/projects.service';
+// import { ProjectDto } from '../dto/project.dto';
+import { ProjectUpdateDto } from '../dto/project-update.dto';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  // @Post('create')
+  // public async createProject(@Body() body: ProjectDto) {
+  //   return await this.projectsService.createProject(body);
+  // }
+
+  @Get('all')
+  public async findAllUsers() {
+    return await this.projectsService.findProjects();
+  }
+
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    return await this.projectsService.findProjectById(id);
+  }
+
+  @Patch('edit/:id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: ProjectUpdateDto,
+  ) {
+    return await this.projectsService.updateProject(body, id);
+  }
+
+  @Delete('delete/:id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.projectsService.deleteProject(id);
+  }
 }
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { UserDto } from '../dto/user.dto';
+import { UserUpdateDto } from '../dto/user-update.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  public async registerUser(@Body() body: UserDto) {
+    return await this.usersService.createUser(body);
+  }
+
+  @Get('all')
+  public async findAllUsers() {
+    return await this.usersService.findUsers();
+  }
+
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    return await this.usersService.findUserById(id);
+  }
+
+  @Patch('edit/:id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: UserUpdateDto,
+  ) {
+    return await this.usersService.updateUser(body, id);
+  }
+
+  @Delete('delete/:id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.usersService.deleteUser(id);
+  }
 }
